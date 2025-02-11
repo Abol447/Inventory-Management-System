b20e445cb47973f16b28f13600bf4c626ee701c9
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-02-11 22:52:45 +0330

    js

--- src/js/createProduct.js
+++ src/js/createProduct.js
@@ -2,14 +2,17 @@ const ProductTitle = document.getElementById('ProductTitle');
 const Quantity = document.getElementById('Quantity');
 const CategorySelection = document.getElementById('CategorySelect');
 const items = document.getElementById('items');
+const addProdctBtn = document.getElementById('addProdctBtn');
 const boxStyle ="p-5  flex justify-between gap-3"; 
 const reperSTyle = "flex gap-2 items-center";
-const categoryStyle = "py-[1px] px-[6px] inline-block border-2 border-slate-400 text-slate-400 rounded-[12px]";
+const categoryStyle = "py-[1px] max-h-[30px] px-[6px] w-[80px] text-center border-2 border-slate-400 text-slate-400 rounded-[12px]";
 const QuantityStyle = "bg-slate-800 rounded-full w-6 flex justify-center items-center  border-2 border-slate-400 text-slate-400 h-6";
-const deletStyle = "py-[1px] px-[6px] inline-block border-2 border-red-400 text-red-400 rounded-[12px]";
-const productCreater = (title,dt,num,category)=>{
+const deletStyle = "cursor-pointer py-[1px] px-[6px] w-[80px] border-2 text-center border-red-400 text-red-400 rounded-[12px]";
+let info = [];
+const productCreater = (title,dt,num,category,id)=>{
     let box = document.createElement('div');
     box.className = boxStyle;
+    box.id = id;
     let name = document.createElement('div');
     name.innerHTML = title;
     let date = document.createElement('div');
@@ -25,6 +28,14 @@ const productCreater = (title,dt,num,category)=>{
     let del = document.createElement('div');
     del.innerHTML = 'delet';
     del.className = deletStyle;
+    del.addEventListener('click',()=>{
+       console.log(box.id)
+        fetch("http://localhost:3000/items/"+box.id,{
+        method :"DELETE"
+       }).then(x => console.log(x))
+         .catch(e => console.log(e))
+        box.remove();
+    })
     raper.appendChild(date);
     raper.appendChild(catg);
     raper.appendChild(number);
@@ -33,4 +44,107 @@ const productCreater = (title,dt,num,category)=>{
     box.appendChild(raper);
     items.appendChild(box);
 }
-productCreater('sdf','sf',5,'sdf');
+addProdctBtn.addEventListener('click',()=>{
+        if(ProductTitle.value.length>0 && Number(Quantity.value)>0){
+            let rnd = Math.floor(Date.now() / 1000);
+            let index = CategorySelection.selectedIndex;
+            let date = new Date
+            productCreater(ProductTitle.value , date.toLocaleDateString(),Number(Quantity.value),CategorySelection[index].innerHTML,rnd);
+            jsonformater(ProductTitle.value , date.toLocaleDateString(),Number(Quantity.value),CategorySelection[index].innerHTML,rnd);
+            fetch("http://localhost:3000/items",{
+                method :"POST",
+                headers :{
+                    "Content-Type": "application/json"
+                },
+                body :JSON.stringify(jsonformater(ProductTitle.value , date.toLocaleDateString(),Number(Quantity.value),CategorySelection[index].innerHTML,rnd))
+            }).then((res) =>  res.json())
+              .then((res) => console.log(res))
+              .catch((err)=> console.log(err))
+        }
+        ProductTitle.value = '';
+        Quantity.value=0;
+        CategorySelection.value = CategorySelection[0].innerHTML
+})
+fetch("http://localhost:3000/items")
+.then((res)=> res.json())
+.then((res)=> {
+    info = res;
+    res.map((x)=>{
+    productCreater(x.title,x.date,x.quantity,x.category,x.id);
+})}).catch((err)=> console.log(err));
+const jsonformater  = (title,dt,num,category,id)=>{
+    let info ={
+        "id":`${id}`,
+        "title" : title,
+        "date" : dt,
+        "quantity": num,
+        "category":category
+    }
+    return info;
+}
+
+
+// <<<<<<<< SEARCH >>>>>>>>>
+
+const searchBtn = document.getElementById('searchBtn');
+const searchProduct = document.getElementById('searchProduct');
+searchBtn.addEventListener('click',()=>{
+    console.log(info)
+    if(searchProduct.value.length>0){
+        info.map((x)=>{
+            if(x.title ==searchProduct.value )
+            {
+                items.innerHTML = '';
+                productCreater(x.title,x.date,x.quantity,x.category,x.id);
+            }
+        })
+    }
+})
+
+
+// <<<<<<<< FILTER >>>>>>>>>
+const SortSelect = document.getElementById('SortSelect')
+SortSelect.addEventListener('change',()=>{
+    mosttoleastsort();
+})
+const NewestSort = ()=>{
+    let arr = [];
+    console.log(info)
+    info.forEach((x)=>{
+        let min = Number(x.id)
+        info.forEach((y)=>{
+            if(arr.indexOf(y.id ) != -1)
+                   return;
+            if(Number(y.id)<min)
+                min = Number(y.id);
+        })
+        arr.unshift(`${min}`)
+    })
+    console.log(arr)
+    reCreatProduct(arr,'id');
+}
+const mosttoleastsort = ()=>{
+    let arr = [];
+    console.log(info)
+    info.forEach((x)=>{
+        let min = Number(x.quantity)
+        info.forEach((y)=>{
+            if(arr.indexOf(y.quantity)!= -1)
+                   return;
+            if(Number(y.quantity)<min)
+                min = Number(y.quantity);
+        })
+        arr.unshift(`${min}`)
+    })
+    console.log(arr)
+    reCreatProduct(arr,'quantity');
+}
+const reCreatProduct = (arr,key)=>{
+    items.innerHTML ='';
+    arr.forEach((x)=>{
+        info.forEach((y)=>{
+            if(x === y[`${key}`])
+                productCreater(y.title,y.date,y.quantity,y.category,y.id);
+        })
+    })
+}
--- src/js/crertcategori.js
+++ src/js/crertcategori.js
@@ -7,18 +7,20 @@ let categoryInfo = [];
 const optionClass = " bg-slate-300 text-slate-900" ;
 fetch("http://localhost:3000/categories")
 .then((x) => x.json())
-.then((x) => x.map((y)=>optCreaterCategory(y.title)))
+.then((x) => x.map((y)=>optCreaterCategory(y.title , y.id)))
 .catch((e)=> console.log(e));
-const optCreaterCategory = (txt)=>{
+const optCreaterCategory = (txt,id)=>{
     let opt = document.createElement('option');
     opt.innerHTML = txt;
     opt.className = optionClass;
+    opt.id =  id;
     CategorySelect.append(opt);
 }
-const send = (title,description)=>{
+const send = (title,description,id)=>{
     let info ={
         "title": `${title}`,
-        "description":`${description}`
+        "description":`${description}`,
+        "id": id
     }
     fetch("http://localhost:3000/categories",{
         method :"POST",
@@ -33,8 +35,9 @@ const send = (title,description)=>{
 btnAddCTG.addEventListener('click',()=>{
     if(addNewCategory.value.length>0 && categoryDescription.value.length>0)
         {
-            optCreaterCategory(addNewCategory.value);
-             send(addNewCategory.value,categoryDescription.value)
+            let rnd = Math.floor(Date.now() / 1000);
+            optCreaterCategory(addNewCategory.value,rnd);
+            send(addNewCategory.value,categoryDescription.value,rnd)
             addNewCategory.value =''
             categoryDescription.value = ''
         }
